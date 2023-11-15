dafbcfeac1e99d111128142f09973ca32c803fb6
Zach21591 <zachary.dangelo@ontariotechu.net>
2023-11-14 22:32:52 -0500

    ee

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,2 @@
+//gives the webpage access to the collection in the mongodb
 module.exports = {"URI":"mongodb://127.0.0.1:27017/SportsTeams"}
--- server/models/Bio_Teams.js
+++ server/models/Bio_Teams.js
@@ -1,12 +1,13 @@
 let mongoose = require('mongoose');
 
-// create a model class
+// create a model class for the sports teams in my mongodb
 let sportsTeamModel = mongoose.Schema({
     TeamName:String,
     Founded:Number,
     KeyPlayers:String
 },
 {
+    //makes the webpage know what collection to look at
     collection: "Teams"
 });
 module.exports = mongoose.model('STeam',sportsTeamModel);
