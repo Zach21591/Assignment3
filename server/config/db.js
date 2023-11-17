6aa06d24a5d9c7f766e71d463f7e80b3a9ab0666
Zach21591 <zachary.dangelo@ontariotechu.net>
2023-11-16 22:46:34 -0500

    ee

--- server/config/db.js
+++ server/config/db.js
@@ -1,2 +1,2 @@
 //gives the webpage access to the collection in the mongodb
-module.exports = {"URI":"mongodb://127.0.0.1:27017/SportsTeams"}
+module.exports = {"URI":"mongodb://0.0.0.0:27017/SportsTeams"}
